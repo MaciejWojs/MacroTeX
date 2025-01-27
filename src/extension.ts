a0c203f5427b63e461805d7144abe72bc42ed207
MaciejWojs <misiek111403@gmail.com>
2025-01-28 00:13:34 +0100

    Update extension name and version; add LaTeX macro configuration and completion support

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,54 @@
 import * as vscode from "vscode";
 
 export const activate = (context: vscode.ExtensionContext) => {
+  console.log("Extension MacroTex is now active!");
+  // Get the configuration for your extension
+  const config = vscode.workspace.getConfiguration('latexMacros');
 
-  vscode.window.showInformationMessage("Test");
-  const disposable = vscode.commands.registerCommand(
-    "bun-vscode-extension.helloworld",
-    () => {
-      vscode.window.showInformationMessage("Hello World!");
-    }
-  );
+  // Get the list of macros
+  const macrosList: { signature: string, extensions: string[] }[] | undefined = config.get('macrosList');
+  if (!macrosList || macrosList.length === 0) {
+    vscode.window.showErrorMessage("No macros defined");
+  } else console.log("Macros loaded", macrosList);
 
+  const disposable = vscode.languages.registerCompletionItemProvider(
+    "latex",
+    {
+      async provideCompletionItems(
+        document: vscode.TextDocument,
+        position: vscode.Position
+      ) {
+        const line = document.lineAt(position).text;
+        let completionItems: vscode.CompletionItem[] = [];
+
+        if (macrosList) {
+          const promises = macrosList.map(async (macro) => {
+            // macro signatutre remove all between [ ]
+            // \fg[asdasdasd]{PATH}{}{}
+
+            const macroSettingsPart = macro.signature.split("PATH");
+            let linePrefix = line.substring(0, position.character)
+              .trim()
+              .replace(/\[.*?\]/g, '[]')
+              .replace(/\{.*?\}/g, '{}');
+            console.log(`${linePrefix} === ${macroSettingsPart[0]} -> ${linePrefix === macroSettingsPart[0]}`);
+            if (linePrefix === macroSettingsPart[0]) {
+              for (const extension of macro.extensions) {
+                const uris = await vscode.workspace.findFiles(`**/*.${extension}`);
+                for (let uri of uris) {
+                  const relativePath = vscode.workspace.asRelativePath(uri.fsPath);
+                  completionItems.push(new vscode.CompletionItem(relativePath));
+                }
+              }
+            }
+          });
+          await Promise.all(promises);
+          return completionItems;
+        }
+
+        return completionItems;
+      },
+    });
   context.subscriptions.push(disposable);
 };
 
